f5fb67ebe48faabe17f77da8ee00261457593419
233333333333 <endxwb@163.com>
2015-06-16 13:00:12 +0800

    SGKTOOL Update v1.5

--- m_Unit.pas
+++ m_Unit.pas
@@ -4,25 +4,24 @@ interface
 
 uses
     Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-    Dialogs, StdCtrls, HtmlParser, IdHTTP, IdBaseComponent, IdComponent,
-    sSkinManager, sEdit, ComCtrls, sListView, sButton, IdTCPConnection,
-    IdTCPClient, Menus, Clipbrd, About, IdAntiFreezeBase, IdAntiFreeze;
+    Dialogs, StdCtrls, HtmlParser, sSkinManager, sEdit, ComCtrls, sListView,
+    sButton, Menus, Clipbrd, About, OverbyteIcsWndControl, OverbyteIcsHttpProt;
 
 type
     Tm_Form = class(TForm)
-        IdHTTP: TIdHTTP;
         sSkinManager: TsSkinManager;
-        sListView: TsListView;
         sEdit: TsEdit;
         sButton: TsButton;
         PopupMenu: TPopupMenu;
         N1: TMenuItem;
         N2: TMenuItem;
-        IdAntiFreeze: TIdAntiFreeze;
+        sListView: TsListView;
+        HttpCli: THttpCli;
         procedure sButtonClick(Sender: TObject);
         procedure N1Click(Sender: TObject);
         procedure sListViewMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
         procedure N2Click(Sender: TObject);
+        procedure FormDestroy(Sender: TObject);
     private
         { Private declarations }
         function MsgBox(const Msg: string; DlgType: TMsgDlgType; Buttons: TMsgDlgButtons; HelpCtx: Integer): Integer;
@@ -40,68 +39,12 @@ implementation
 function Tm_Form.MsgBox(const Msg: string; DlgType: TMsgDlgType; Buttons: TMsgDlgButtons; HelpCtx: Integer): Integer;
 begin
     with CreateMessageDialog(Msg, DlgType, Buttons) do
-    try
-        Position := poOwnerFormCenter;
-        Result := ShowModal
-    finally
-        Free
-    end
-end;
-
-procedure Tm_Form.N1Click(Sender: TObject);
-begin
-    Clipbrd.Clipboard.AsText := sListView.Selected.Caption + ' | ' + sListView.Selected.SubItems.Strings[0] + ' | ' + sListView.Selected.SubItems.Strings[1] + ' | ' + sListView.Selected.SubItems.Strings[2];
-    MsgBox(Clipbrd.Clipboard.AsText, mtInformation, [mbOK], 0);
-end;
-
-procedure Tm_Form.N2Click(Sender: TObject);
-begin
-    AboutBox.ShowModal;
-end;
-
-procedure Tm_Form.sButtonClick(Sender: TObject);
-var
-    HTTP: TIdHTTP;
-    i, j: Integer;
-    tr, td: IHtmlElementList;
-    ParamPost: TStringList;
-    ResponseStream: TStringStream;
-begin
-    HTTP := TIdHTTP.Create(nil);
-    ParamPost := TStringList.Create;
-    ResponseStream := TStringstream.Create('', TEncoding.UTF8);
-    try
-        sButton.Enabled := False;
-        ParamPost.Add('type=1');
-        ParamPost.Add('key=' + Trim(sEdit.Text));
-        HTTP.Post('http://shota.cc/tools/qun.php', ParamPost, ResponseStream);
-        tr := ParserHTML(ResponseStream.DataString).SimpleCSSSelector('table tbody tr');
-        if tr.Count = 0 then
-            MsgBox('未能找到该QQ相关信息或请求过于频繁！', mterror, [mbOk], 0)
-        else
-        begin
-            sListView.Items.Clear;
-            for i := 0 to tr.Count - 1 do
-            begin
-                sListView.Items.BeginUpdate;
-                td := ParserHTML(tr[i].InnerHtml).SimpleCSSSelector('td');
-                for j := 0 to td.Count div 4 - 1 do
-                begin
-                    sListView.Items.Add.Caption := td[1].InnerText;
-                    sListView.Items[sListView.Items.Count - 1].SubItems.Add(td[2].InnerText);
-                    sListView.Items[sListView.Items.Count - 1].SubItems.Add(td[3].InnerText);
-                    sListView.Items[sListView.Items.Count - 1].SubItems.Add(td[4].InnerText);
-                end;
-                sListView.Items.EndUpdate;
-            end
-        end;
-    finally
-        FreeAndNil(HTTP);
-        ParamPost.Free;
-        ResponseStream.Free;
-        sListView.Refresh;
-        sButton.Enabled := True;
-    end;
+        try
+            Position := poOwnerFormCenter;
+            Result := ShowModal
+        finally
+            Free
+        end
 end;
 
 procedure Tm_Form.sListViewMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
@@ -120,6 +63,71 @@ begin
             N1.Visible := False;
             PopupMenu.Popup(P.X, P.Y);
         end;
+end;
+
+procedure Tm_Form.N1Click(Sender: TObject);
+begin
+    Clipbrd.Clipboard.AsText := sListView.Selected.Caption + ' | ' + sListView.Selected.SubItems.Strings[0] + ' | ' + sListView.Selected.SubItems.Strings[1] + ' | ' + sListView.Selected.SubItems.Strings[2];
+    MsgBox(Clipbrd.Clipboard.AsText, mtInformation, [mbOK], 0);
+end;
+
+procedure Tm_Form.N2Click(Sender: TObject);
+begin
+    AboutBox.ShowModal;
+end;
+
+procedure Tm_Form.FormDestroy(Sender: TObject);
+begin
+    FreeAndNil(HttpCli);
+end;
+
+procedure Tm_Form.sButtonClick(Sender: TObject);
+var
+    i, j: Integer;
+    Buf: AnsiString;
+    InStream: TMemoryStream;
+    OutStream: TStringStream;
+    tr, td: IHtmlElementList;
+begin
+    sButton.Enabled := False;
+    InStream := TMemoryStream.Create;
+    OutStream := TStringstream.Create('', TEncoding.UTF8);
+    Buf := 'type=1&key=' + Trim(sEdit.Text);
+    InStream.Write(buf[1], Length(Buf));
+    InStream.Position := 0;
+    HttpCli.SendStream := InStream;
+    HttpCli.RcvdStream := OutStream;
+    HttpCli.URL := 'http://shota.cc/tools/qun.php';
+    try
+        HttpCli.Post;
+        tr := ParserHTML(OutStream.DataString).SimpleCSSSelector('table tbody tr');
+        if tr.Count = 0 then
+            MsgBox('未能找到该QQ相关信息或请求过于频繁！', mterror, [mbOk], 0)
+        else
+        begin
+            sListView.Items.Clear;
+            for i := 0 to tr.Count - 1 do
+            begin
+
+                td := ParserHTML(tr[i].InnerHtml).SimpleCSSSelector('td');
+                for j := 0 to td.Count div 4 - 1 do
+                begin
+                    sListView.Items.BeginUpdate;
+                    sListView.Items.Add.Caption := td[1].InnerText;
+                    sListView.Items[sListView.Items.Count - 1].SubItems.Add(td[2].InnerText);
+                    sListView.Items[sListView.Items.Count - 1].SubItems.Add(td[3].InnerText);
+                    sListView.Items[sListView.Items.Count - 1].SubItems.Add(td[4].InnerText);
+                    sListView.Items.EndUpdate;
+                end;
+
+            end
+        end;
+    finally
+        InStream.Free;
+        OutStream.Free;
+        sListView.Refresh;
+        sButton.Enabled := True;
+    end;
 end;
 
 end.
